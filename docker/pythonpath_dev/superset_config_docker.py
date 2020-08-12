af32cc4db0bcaf0a0995a3212de341cbeca54a18
telpa <goodayjack@gmail.com>
2020-08-12 00:00:32 +0000

    Change cache refresh time

--- docker/pythonpath_dev/superset_config_docker.py
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -33,7 +33,7 @@
 # FileSystemCache (more notes there)
 CACHE_CONFIG = {
     'CACHE_TYPE': 'redis',
-    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 24, # 1 day default (in secs)
+    'CACHE_DEFAULT_TIMEOUT': 60 * 60 * 6, # 6 hours
     'CACHE_KEY_PREFIX': 'superset_results',
     'CACHE_REDIS_URL': 'redis://redis:6379/0',
 }
